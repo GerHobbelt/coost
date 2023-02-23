382364f59dcf872040468ea741b7699934e9f785
Ger Hobbelt <ger@hobbelt.com>
2023-02-23 20:55:09 +0100

    fix duplicated here after code review.
    ----------------------
    
    tesseract: fix compiler errors due to windows system header file errors
    
    // mupdf headers cause some weird error in MSVC system header commdlg.h when include *after* <random> header below. And this only happens for params.cpp, i.e. when params.h has been included first. ... A definite case of WTF?!
    //
    // Which is why we include the mupdf headers here in monolithic builds...
    //
    // EDIT: Subsequent compiler runs and analysis now popped up the same 'crazy' errors in commdlg.h (caused by missing font struct definitions)
    // from control.cpp and a few other tesseract source files. Which forced me to investigate further.
    //
    // Debugging through running the preprocessor (cl /E /P ...) and some grepping
    //
    //   grep '#line' $( find -name control.i )  | grep -B 1000000 commdlg | grep -B 1000000 wingdi | grep -v "Program Files"
    //
    // showed the original culprit was probably MuPDF\\thirdparty\\curl\\lib\\setup-win32.h.
    // And indeed there we found the often-troublesome WIN32_LEAN_AND_MEAN and a few choice other NOXYZ feature defines before loading windows.h.
    //
    // > Rationale for the precise grep chain is out of scope.
    // > Hint: wingdi defines what commdlg needs. Chain + last filter takes care of getting loading file as last #line stmt, IFF you're lucky.
    // > Of course I was lucky. After N iterations, which got me to this grep chain. EFF that shite, with prejudice!
    //
    // This practice MUST be abolished in all libraries, everywhere, as it causes severe compile errors at surprise locations and at surprise times,
    // while the errors reported aren't always easy to diagnose for everybody. (How many programmers are well versed with gcc -E, cl /P and code inspection?)
    //
    // Setting these feature defines MUST be the sole prerogative of the final application code/project, if any. Or rather more precise: the final C/C++ *.c+*.cpp source files.
    // No-one else MUST mess with these in any header / include files, just to 'help' shorten compiler turn-around times. The road to Hell is paved with good intentions.
    // If you want to offer 'help' like that, consider making sure your header files work well with precompiled header caching in the various compilers instead.
    //
    
    See also cUrl repo.

--- include/co/thread.h
+++ include/co/thread.h
@@ -7,9 +7,9 @@
 #include <memory>
 
 #ifdef _WIN32
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif
+//#ifndef WIN32_LEAN_AND_MEAN
+//#define WIN32_LEAN_AND_MEAN
+//#endif
 #include <windows.h>
 
 namespace co {
